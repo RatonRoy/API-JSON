095c6fc4a1aa0b7140d72287b74e377b682b872b
RatonRoy <royraton18@gmail.com>
2022-02-26 02:56:01 +0600

    every thing is ok

--- meal.js
+++ meal.js
@@ -10,7 +10,23 @@ document.getElementById('meal-btn').addEventListener('click', function () {
 })
 const mealsDisplay = (name) => {
 	const meals = name.meals;
+	const mealsContainer = document.getElementById('meals-container');
 	for (const meal of meals) {
-		console.log(meal);
+		console.log(meal.idMeal);
+		const div = document.createElement('div');
+		div.classList.add('col');
+		div.innerHTML = `
+			<div class="card">
+					<img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">${meal.strMeal}</h5>
+						<p class="card-text" style="text-align: justify;">${meal.strInstructions.slice(0, 200)}</p> 
+					</div>
+			</div>
+		`
+		mealsContainer.appendChild(div);
 	}
+}
+const mealDetiles = meal => {
+
 }
