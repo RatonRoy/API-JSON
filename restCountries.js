64a6ed606cb7cc0b968429ffa2caa40597d76994
RatonRoy <royraton18@gmail.com>
2022-02-25 23:05:50 +0600

    complete the button informations

--- restCountries.js
+++ restCountries.js
@@ -11,12 +11,20 @@ const displayRestCountries = (countries) => {
 		// console.log(countrie.capital + ' ' + countrie.borders);
 		const div = document.createElement('div');
 		div.classList.add('country')
-		const h2 = document.createElement('h2');
+		div.innerHTML = `
+			<h2> Name : ${countrie.name.common} </h2> 
+			<p> Capital : ${countrie.capital} </p> 
+			<button onclick="restCountriesByName('${countrie.name.common}');" class="btn"> Details </button>
+		`
+		/* const h2 = document.createElement('h2');
 		h2.innerText = `Name : ${countrie.name.common}`;
 		div.appendChild(h2);
 		const p = document.createElement('p');
 		p.innerText = ` Capital : ${countrie.capital}`;
-		div.appendChild(p);
+		div.appendChild(p); */
 		conuntryContainer.appendChild(div);
 	});
+}
+const restCountriesByName = (name) => {
+	console.log(name)
 }
